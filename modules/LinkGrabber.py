4e07e32ed4425e86192b08ffe5b4f5236da289b8
maxpiorischin <maxpiorischin@hotmail.com>
2021-05-05 01:32:49 -0400

    minor helpful changes

--- modules/LinkGrabber.py
+++ modules/LinkGrabber.py
@@ -3,11 +3,20 @@ from urllib.request import Request, urlopen
 import re
 from bs4 import BeautifulSoup as Soup
 from googlesearch import search
+import json
 
 ytsearch_url = "https://www.youtube.com/results?search_query="
 video_url = "https://www.youtube.com/watch?v="
-google_images_url = "https://www.google.com/search?tbm=isch&q="
+google_images_url = "https://www.google.co.in/search?q="
+google_images_url_end = "&source=lnms&tbm=isch"
 google_url = "https://www.google.ca/search?q="
+
+REQUEST_HEADER = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+
+
+def get_soup(url, header):
+    return Soup(urlopen(Request(url, headers=header)), 'html.parser')
 
 
 def linkcreator(url, id):
@@ -22,13 +31,11 @@ def videograbber(searchterm):
 
 
 def imagegrabber(searchterm):
-    final_url = google_images_url + searchterm
+    final_url = google_images_url + searchterm + google_images_url_end
     print(final_url)
-    request = Request(final_url, headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = urlopen(request)
-    html = Soup(webpage, 'html.parser')
-    imgs = [img['src'] for img in html.find_all('img')]
-    # print(imgs[1])
+    soup = get_soup(final_url, REQUEST_HEADER)
+    imgs = [img['src'] for img in soup.find_all('img')]
+
     return imgs[1]
 
 
@@ -36,4 +43,4 @@ def googlesearch(searchterm):
     result = search(searchterm, num_results=3)
     return result[0]
 
-print(googlesearch("wiki"))
+print(imagegrabber("pewdiepie"))
