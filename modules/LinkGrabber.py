f533ce5cc5104eb5be11f7c6cbe24cc77c027c8d
maxpiorischin <maxpiorischin@hotmail.com>
2021-05-05 03:09:36 -0400

    speed test

--- modules/LinkGrabber.py
+++ modules/LinkGrabber.py
@@ -6,6 +6,15 @@ from googlesearch import search
 from selenium import webdriver
 import os
 
+option = webdriver.ChromeOptions()
+
+option.binary_location = os.getenv('GOOGLE_CHROME_BIN')
+
+option.add_argument("--headless")
+option.add_argument('--disable-gpu')
+option.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path=os.getenv('CHROME_EXECUTABLE_PATH'), options=option)
+
 ytsearch_url = "https://www.youtube.com/results?search_query="
 video_url = "https://www.youtube.com/watch?v="
 google_images_url = "https://www.google.co.in/search?q="
@@ -34,14 +43,6 @@ def videograbber(searchterm):
 
 
 def imagegrabber(searchterm):
-    option = webdriver.ChromeOptions()
-
-    option.binary_location = os.getenv('GOOGLE_CHROME_BIN')
-
-    option.add_argument("--headless")
-    option.add_argument('--disable-gpu')
-    option.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path=os.getenv('CHROME_EXECUTABLE_PATH'), options=option)
 
     url = google_images_url + searchterm + google_images_url_end
     driver.get(url)
