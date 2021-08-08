59014b53341ffc0539cb241321f922eec6a908c0
maxpiorischin <maxpiorischin@hotmail.com>
2021-08-08 04:40:56 -0400

    data to mongo added

--- testt.py
+++ testt.py
@@ -3,6 +3,25 @@ import os
 cluster = MongoClient(os.getenv("MONGODB_CONNECTION"), connect = False)
 print(cluster)
 db = cluster["ChadStats"]
-commands = db["Commands"]
+IMG = db["IMG"]
+Misc = db["Misc"]
+WebSearch = db["WebSearch"]
+Youtube = db["Youtube"]
 
-commands.renameCollection("IMG")
+for i in range(2800):
+    post = {"_id": "img" + str(i), "command": "img", "date": 2021, "content": "before stats", "user": "before stats"}
+    IMG.insert_one(post)
+
+for i in range(600):
+    post = {"_id": "misc" + str(i), "command": "misc", "date": 2021, "content": "before stats", "user": "before stats"}
+    Misc.insert_one(post)
+
+for i in range(100):
+    post = {"_id": "yt" + str(i), "command": "yt", "date": 2021, "content": "before stats", "user": "before stats"}
+    Youtube.insert_one(post)
+
+for i in range(130):
+    post = {"_id": "search" + str(i), "command": "define", "date": 2021, "content": "before stats", "user": "before stats"}
+    WebSearch.insert_one(post)
+
+print("done")
