8beb243c083c699bd5afcace4227bb6a0a2877f2
maxpiorischin <maxpiorischin@hotmail.com>
2021-08-08 04:28:44 -0400

    rename collection

--- testt.py
+++ testt.py
@@ -5,8 +5,4 @@ print(cluster)
 db = cluster["ChadStats"]
 commands = db["Commands"]
 
-for i in range(2800):
-    post = {"_id": "img" + str(i), "command": "img", "date": 2021, "content": "before stats", "user": "before stats"}
-    commands.insert_one(post)
-
-print("done")
+commands.renameCollection("IMG")
