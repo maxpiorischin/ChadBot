c7bb1bf821f1af0389833cb67b7280546a9e0b4b
maxpiorischin <maxpiorischin@hotmail.com>
2021-08-08 04:02:14 -0400

    bug fix

--- testt.py
+++ testt.py
@@ -5,7 +5,7 @@ print(cluster)
 db = cluster["ChadStats"]
 commands = db["Commands"]
 
-post = { "_id": "img", "date": 2021, "content": "before stats", "user": "before stats" }
+post = { "command": "img", "date": 2021, "content": "before stats", "user": "before stats" }
 for i in range(2800):
     commands.insert_one(post)
 
