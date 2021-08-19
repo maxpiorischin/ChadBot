03a06066a20d2731a39f660d2c09ecec33231943
maxLaptop <maxpiorischin@hotmail.com>
2021-08-19 01:35:39 -0400

    reddit test

--- cogs/Reddit.py
+++ cogs/Reddit.py
@@ -27,13 +27,23 @@ class Reddit(commands.Cog):
     async def reddit(self, ctx, *search):
         search_term = ''.join(search)
         print("searching reddit: " + search_term)
-        try:
-            subreddit = await self.reddit.subreddit(search_term)
-            async for submission in subreddit.hot(limit=10):
-                await ctx.send(submission.title)
-        except:
-            embed = tools.embed_creator("ERROR", "Subreddit does not exist", discord.Color.red())
-            ctx.send("", embed = embed)
+        last_val = search_term[len(search_term.rstrip('0123456789')):]
+        if not last_val.isdigit():
+            last_val = 1
+        if int(last_val) > 10:
+            embed = tools.embed_creator("ERROR", "Maximum of 10 Images!", discord.Color.red())
+            await ctx.send("", embed=embed)
+            return
+        if (search_term.endswith("," + last_val)) and 0 < int(last_val) <= 10:
+            search_term = search_term[:-(len(last_val) + 2)]
+            print(search_term)
+            try:
+                subreddit = await self.reddit.subreddit(search_term)
+                async for submission in subreddit.hot(limit=last_val):
+                    await ctx.send(submission.title)
+            except:
+                embed = tools.embed_creator("ERROR", "Subreddit does not exist", discord.Color.red())
+                await ctx.send("", embed = embed)
         await self.MongoWorker.add_reddit("reddit", search_term, ctx.message.author, ctx.message.guild)
 
 
