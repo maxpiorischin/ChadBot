2b96a37c5a4f73cd8ffd0fd3090c733752442b76
maxpiorischin <maxpiorischin@hotmail.com>
2021-11-21 02:37:10 -0500

    better awaiting

--- modules/Mongo.py
+++ modules/Mongo.py
@@ -16,32 +16,32 @@ class MongoWorker:
         self.Youtube = self.dbchadstats["Youtube"]
 
     async def get_prefix(self, message):
-        post = self.prefixes.find_one({"_id": str(message.guild.id)})
+        post = await self.prefixes.find_one({"_id": str(message.guild.id)})
         return post["prefix"]
 
     async def add_prefix(self, id, prefix):
         post = {"_id": str(id), "prefix": prefix}
-        self.prefixes.insert_one(post)
+        await self.prefixes.insert_one(post)
 
     async def del_prefix(self, id):
-        self.prefixes.delete_one({"_id": str(id)})
+        await self.prefixes.delete_one({"_id": str(id)})
 
     async def update_prefix(self, id, prefix):
-        self.prefixes.update_one({"_id": str(id)}, {"$set": {"prefix": prefix}})
+        await self.prefixes.update_one({"_id": str(id)}, {"$set": {"prefix": prefix}})
 
     async def add_img(self, content, user, guild):
         post = {"_id": datetime.now(), "command": "img", "content": content,
                 "user": user.name + "#" + user.discriminator, "user_id" : user.id, "time" : str(date.today()), "server_name" : guild.name, "server_id" : guild.id}
-        self.IMG.insert_one(post)
+        await self.IMG.insert_one(post)
     async def add_misc(self, command, content, user, guild):
         post = {"_id": datetime.now(), "command": command,"content": content,
                 "user": user.name + "#" + user.discriminator, "user_id" : user.id, "time" : str(date.today()), "server_name" : guild.name, "server_id" : guild.id}
-        self.Misc.insert_one(post)
+        await self.Misc.insert_one(post)
     async def add_web(self, command, content, user, guild):
         post = {"_id": datetime.now(), "command": command, "content": content,
                 "user": user.name + "#" + user.discriminator, "user_id" : user.id, "time" : str(date.today()), "server_name" : guild.name, "server_id" : guild.id}
-        self.WebSearch.insert_one(post)
+        await self.WebSearch.insert_one(post)
     async def add_youtube(self, command, content, user, guild):
         post = {"_id": datetime.now(), "command": command, "content": content,
                 "user": user.name + "#" + user.discriminator, "user_id" : user.id, "time" : str(date.today()), "server_name" : guild.name, "server_id" : guild.id}
-        self.Youtube.insert_one(post)
+        await self.Youtube.insert_one(post)
