9e7b67240ff82b921e60a82588166268634aaa0d
maxLaptop <maxpiorischin@hotmail.com>
2022-02-04 21:33:49 -0500

    test

--- cogs/Google.py
+++ cogs/Google.py
@@ -107,7 +107,7 @@ class Google(commands.Cog):
             await ctx.reply(content=link, mention_author=False)
 
         if not sent:
-            return await ctx.embed(title="No Results Found!", color=invis)
+            return await ctx.embed(title='No Results Found! Try the "im" command instead!', color=invis)
 
             # await ctx.embed(
             #     image_url=link, footer={"text": f"search term: {content}"}, color=invis
