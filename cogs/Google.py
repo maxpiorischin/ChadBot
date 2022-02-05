1f9731ea92a399611c2aa243da088bf8f0560369
Maxim Piorischin <60104503+maxpiorischin@users.noreply.github.com>
2022-02-04 21:13:03 -0500

    Added warning when gurgle not working

--- cogs/Google.py
+++ cogs/Google.py
@@ -93,7 +93,7 @@ class Google(commands.Cog):
         await self.MongoWorker.add_img(content, ctx.message.author, ctx.message.guild)
 
         if len(links) == 0:
-            return await ctx.embed(title="No Results Found!", color=invis)
+            return await ctx.embed(title='No Results Found! Try the "im" command instead!', color=invis)
 
         i = 0
         sent = False
